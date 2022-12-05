7fe7232bdc6e4abef240dc66d632b336382eae3e
DarraghMadden <darraghmadden7@gmail.com>
2022-12-05 17:05:46 +0000

    removes space

--- JS/login.js
+++ JS/login.js
@@ -25,7 +25,7 @@ function getUser() {
     GSU = 'https://prod-00.northeurope.logic.azure.com/workflows/f713b11eb4db46b7a0758abeabd40cd0/triggers/manual/paths/invoke/api/v1/users/' + username + '/' + password + '?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=gbzBE5D0R267GWaJUnhxSMYSp2QZ7apFRzdqwE4V1ts';
 
     $.getJSON(GSU, function( data ) {
-        sessionStorage.setItem('isAdmin', data.ResultSets.Table1[0].isAdmin )
+        sessionStorage.setItem('isAdmin', data.ResultSets.Table1[0].isAdmin)
         sessionStorage.setItem('username', data.ResultSets.Table1[0].username)
     });
 }
